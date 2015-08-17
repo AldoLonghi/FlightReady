3bbf15110713cd129add6e913ebbfb7c816791c1
Aldo Longhi <aldo.d.longhi@gmail.com>
2015-08-17 00:02:58 -0700

    Add an event listener to execute a PRAGMA statement for every new database connection. Apparently SQLite3 doesn't automatically enforce foreign key constraints; You have to turn it on.

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,21 @@ db = SQLAlchemy(app)            # Setup Flask-SQLAlchemy
 manager = Manager(app)          # Setup Flask-Script
 
 from app.startup.init_app import create_app
+
+
+# The following was added so that every new connection has the command
+# "PRAGMA foreign_keys = ON" executed.  Apparently there's no better way.
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+from sqlite3 import Connection as SQLite3Connection
+
+# Force sqllite contraint foreign keys - Must be done for each new connection!
+@event.listens_for( Engine, "connect" )
+def set_sqlite_pragma( dbapi_connection, connection_record ):
+	"""
+	Force sqllite contraint foreign keys
+	"""
+	if isinstance( dbapi_connection, SQLite3Connection ):
+		cursor = dbapi_connection.cursor()
+		cursor.execute( "PRAGMA foreign_keys = ON" )
+		cursor.close()
